885879baada43f79a5698cd7fc27d47dafaa6dbc
zullasii <161367090+zullasii@users.noreply.github.com>
2026-08-27 01:44:09 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Настройки бота. Впиши свои значения ниже.
 
 # Токен бота, полученный у @BotFather
-BOT_TOKEN = "8278437697:AAFB3GGf_036u7UGOFotQ29kfojkqzg8CPo"
+BOT_TOKEN = ""
 
 # Адрес и порт Minecraft сервера (Java Edition)
 MC_HOST = "S10.ipnodns.ru"
